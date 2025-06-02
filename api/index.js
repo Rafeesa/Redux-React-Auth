f55d1c08a51cdb9443b7cef3bbdf0aba8a43afcd
rafeesa <rafeesapc00@gmail.com>
2025-06-02 06:39:09 +0530

    added middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -18,6 +18,17 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute); 
 
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode||500
+    const message=err.message||"internal server error"
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+
+})
+
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
