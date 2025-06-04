5bd9391c964241e1ded7422772fdc8155b36a4ad
rafeesa <rafeesapc00@gmail.com>
2025-06-04 08:08:34 +0530

    profile updation completed

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema=new mongoose.Schema({
         required:true
         
     },
+     profilePicture: {
+      type: String,
+     
+    },
 
 },{timestamps:true});
 
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -65,7 +65,7 @@ export default function Profile() {
      console.log(error)
     }
   };
-
+console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
